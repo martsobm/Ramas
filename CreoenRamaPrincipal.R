afb5d4353296140adde0a81e2fba6c6fa014c99a
martsobm <marcelino.martinez@uv.es>
2024-10-15 17:44:24 +0200

    commit antes de fusionar

--- CreoenRamaPrincipal.R
+++ CreoenRamaPrincipal.R
@@ -2,3 +2,7 @@
 x=2
 
 # Al volver a la rama principal el fichero creado en la rama 1 se cierra y ya no es visible
+
+# Al volver a pricipal deja de verse el pdf que había añadido desde la rama.
+
+#Fusionao Rama 1 y principal
